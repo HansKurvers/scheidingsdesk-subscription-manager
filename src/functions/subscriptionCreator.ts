9cc73ba096cc5ea77bb366804e52913aba0e4c1a
HansKurvers <hans@scheidingsdesk.nl>
2025-04-08 14:32:49 +0200

    Add redirect URL

--- src/functions/subscriptionCreator.ts
+++ src/functions/subscriptionCreator.ts
@@ -7,6 +7,8 @@ import createMollieClient, { SequenceType } from '@mollie/api-client';
 
 // Mollie API configuration
 const MOLLIE_API_KEY = process.env.MOLLIE_API_KEY as string;
+const mollieRedirect = process.env.MOLLIE_REDIRECT_URL as string;
+
 
 const WEBHOOK_URL = process.env.WEBHOOK_URL;
 
@@ -44,7 +46,7 @@ async function initializeSubscription(request: HttpRequest, context: InvocationC
         const paymentResponse = await mollieClient.payments.create({customerId: customer.id, amount: {
             currency: "EUR",
             value: amount || "0.01"
-        }, sequenceType: SequenceType.first, description: 'Eerste betaling'})
+        }, sequenceType: SequenceType.first, description: 'Eerste betaling', redirectUrl: mollieRedirect})
         
         // Return success response with payment details
         return {
